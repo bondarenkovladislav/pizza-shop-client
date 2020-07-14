d0e38ee279e73e79d2adbd3442999b1dbae2769e
vladislavbondarenko <bondarencko40@gmail.com>
2020-07-14 20:12:39 +0300

    API root change to deployed backend

--- src/core/ApiConfig.ts
+++ src/core/ApiConfig.ts
@@ -3,7 +3,7 @@ import { IProduct } from '../interfaces/IProduct'
 import { IIngredient } from '../interfaces/IIngredient'
 import { IOrder } from '../interfaces/IOrder'
 
-const API_ROOT = 'http://localhost:5000'
+const API_ROOT = 'https://vibrant-croissant-02483.herokuapp.com'
 
 export type ApiCall<P, T, B = void> = (
   urlParams?: P,
